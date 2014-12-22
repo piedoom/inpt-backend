b1b102279b09deb7b487e793346f602af929ebb2
steinomatic <dmstein@terpmail.umd.edu>
2014-12-21 22:55:30 -0500

    added json generation

--- www/inptadmin/config.php
+++ www/inptadmin/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-//Leave as ../ if the Inpt backend is installed to the same directory as your site
+//Leave as .. if the Inpt backend is installed to the same directory as your site
 $inpt_conf['BASE_DIR'] = '..';
 
 //So Inpt doesn't scan itself unnecessarily
--- www/inptadmin/inpt.php
+++ www/inptadmin/inpt.php
@@ -14,18 +14,74 @@ class Inpt
 
 	function scan()
 	{
-		$inpt_match_regex = '/<[a-zA-Z]+ .* ?class=["|\'].* ?inpt.* ?["|\'] ?.*>/m';
 		$files = $this->enumerate_files($this->config['BASE_DIR']);
 		$inpt_files = array();
 		
 		foreach ($files as $file) {
-			if (preg_match($inpt_match_regex, file_get_contents($file)))
+			if (count($this->get_inpt(file_get_contents($file))) > 0)
 			{
 				$inpt_files[] = $file;
 			}
 		}
 
 		return $inpt_files;
+	}
+
+	function get_inpt($html)
+	{
+		$dom = new DOMDocument;
+		$dom->preserveWhiteSpace = true;
+		$dom->formatOutput = true;
+		@$dom->loadHTML($html);
+
+		$comment = null;
+		$inptNodes = array();
+
+
+		//TODO: Clean this up, cause There Has To Be A Better Way.
+		foreach($dom->documentElement->childNodes as $node)
+		{
+			if ($node->nodeName == 'body')
+			{
+				foreach($node->childNodes as $innerNode)
+				{
+					if ($innerNode->nodeName == '#comment')
+					{
+						$comment = $dom->saveHTML($innerNode);
+					}
+					else
+					{
+						if (isset($innerNode->attributes))
+						{
+							if (null != ($innerNode->attributes->getNamedItem('class')))
+							{
+								if ($innerNode->attributes->getNamedItem('class')->nodeValue == 'inpt')
+								{
+									$inpt_node = new InptBlock();
+
+									if ($comment != null)
+									{
+										$inpt_node->metadata = $this->parse_metadata($comment);
+										$inpt_node->name = isset($inpt_node->metadata['inpt-title']) ? $inpt_node->metadata['inpt-title'] : "";
+									}
+
+									$inpt_node->contents = $innerNode->nodeValue;
+
+									$inptNodes[] = $inpt_node;
+								}
+							}
+						}
+
+						if ($innerNode->nodeName != '#text')
+						{
+							$comment = null;
+						}
+					}
+				}
+			}
+		}
+
+		return $inptNodes;
 	}
 
 	function enumerate_files($dir)
@@ -49,14 +105,87 @@ class Inpt
 
 		return $files;
 	}
+
+	function parse_metadata($str)
+	{
+		$str = trim($str, '<>!-');
+		$metadata = array();
+		$str = explode("\n", $str);
+
+		foreach ($str as $line)
+		{
+			$line = explode(':', $line);
+
+			if (count($line) == 2)
+			{
+				$metadata[trim($line[0])] = trim($line[1]);
+			}
+		}
+
+		return $metadata;
+	}
+
+	function output_json()
+	{
+		$json = array();
+		$files = $this->scan();
+
+		foreach($files as $file)
+		{
+
+			//Figure out page_name
+			$filepath = explode('/', $file);
+			$filename = $filepath[count($filepath) - 1];
+			$filefolder = $filepath[count($filepath) - 2];
+
+			if ($filename == 'index.html')
+			{
+				if ($filefolder == '..')
+				{
+					$page_name = 'Home';
+				}
+				else
+				{
+					$page_name = ucfirst($filefolder);
+				}
+			}
+			else
+			{
+				$title_regex = '/<title>(.*)<\/title>/m';
+				preg_match($title_regex, file_get_contents($file), $matches);
+
+				if (count($matches) > 1)
+				{
+					$page_name = ucfirst($matches[1]);
+				}
+				else
+				{
+					$filename2 = explode('.', $filename);
+					$page_name = ucfirst($filename2[0]);
+				}
+
+			}
+
+			//Fetch inpt editable sections and parse metadata
+			$sections = $this->get_inpt(file_get_contents($file));
+
+			//Build up json
+			$json[$file] = array('page_name' => $page_name, 'page_sections' => $sections);
+		}
+
+		header('Content-Type: application/json');
+		echo(json_encode($json));
+		die();
+	}
+}
+
+
+class InptBlock
+{
+	var $name = "";
+	var $contents = "";
+	var $metadata = array();
 }
 
 $inpt = new Inpt();
-$files = $inpt->scan();
-
-echo "Locating Inpt Files: <br />\n";
-
-foreach($files as $file)
-{
-	echo sprintf("%s <br />\n", $file);
-}
+$files = $inpt->output_json();
